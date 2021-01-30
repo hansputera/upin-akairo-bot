09d0d4da3ef26fd9c62a62ca43d67d2e335f035c
hansputera <hanifdwyputrasembiring@gmail.com>
2021-01-30 17:46:24 +0700

    delete: compile result

--- dist/commands/Music/PlayCommand.js
+++ dist/commands/Music/PlayCommand.js
@@ -15,6 +15,10 @@ class PlayCommand extends discord_akairo_1.Command {
                         retry: "Please type song title, (eg. One Direction Memories)",
                         retries: 5
                     }
+                }, {
+                    id: "queryFlag",
+                    match: "flag",
+                    flag: ['--soundcloud', '-sc']
                 }],
             editable: false,
             clientPermissions: ['CONNECT', 'SPEAK'],
@@ -23,13 +27,18 @@ class PlayCommand extends discord_akairo_1.Command {
         });
         this.client = client;
     }
-    async exec(message, { track }) {
+    async exec(message, { track, queryFlag }) {
         if (!message.member.voice.channel)
             return message.util.reply('Please join to voice channel!');
         const queue = this.client.musicManager.queue.get(message.guild.id);
         if (queue && queue.voiceChannel !== message.member.voice.channel)
             return message.util.reply('Please join to my voice channel!');
-        const tracks = await this.client.musicManager.getSongs(`ytsearch:${track}`);
+        let query;
+        if (queryFlag)
+            query = "scsearch";
+        else
+            query = "ytsearch";
+        const tracks = await this.client.musicManager.getSongs(`${query}:${track}`);
         if (!tracks.length)
             return message.util.reply('I can\'t found that song!');
         await this.client.musicManager.handleVideo(message, message.member.voice.channel, tracks[0]);
