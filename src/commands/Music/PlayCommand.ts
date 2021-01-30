251228f77f6fa00b2e4fbf47e78b0552b22cec0b
hansputera <hanifdwyputrasembiring@gmail.com>
2021-01-30 17:45:17 +0700

    add: soundcloud search flag

--- src/commands/Music/PlayCommand.ts
+++ src/commands/Music/PlayCommand.ts
@@ -16,6 +16,10 @@ export default class PlayCommand extends Command {
                     retry: "Please type song title, (eg. One Direction Memories)",
                     retries: 5
                 }
+            }, {
+                id: "queryFlag",
+                match: "flag",
+                flag: ['--soundcloud', '-sc']
             }],
             editable: false,
             clientPermissions: ['CONNECT', 'SPEAK'],
@@ -24,11 +28,15 @@ export default class PlayCommand extends Command {
         })
     }
 
-    public async exec(message: Message, { track }: { track: string; }) {
+    public async exec(message: Message, { track, queryFlag }: { track: string; queryFlag: boolean; }) {
         if (!message.member!.voice.channel) return message.util!.reply('Please join to voice channel!');
         const queue = this.client.musicManager.queue.get(message.guild!.id);
         if (queue && queue!.voiceChannel !== message.member!.voice.channel) return message.util!.reply('Please join to my voice channel!');
-        const tracks = await this.client.musicManager.getSongs(`ytsearch:${track}`);
+        let query: string | undefined;
+        if (queryFlag) query = "scsearch";
+        else query = "ytsearch";
+        
+        const tracks = await this.client.musicManager.getSongs(`${query}:${track}`);
         if (!tracks.length) return message.util!.reply('I can\'t found that song!');
         await this.client.musicManager.handleVideo(message, message.member!.voice.channel!, tracks[0]);
     }
